5992f5a4297f2455e8e20aeb67efe407433d72ed
Fred Hajdarpasic <fred.hajdarpasic@outlook.com>
2021-03-17 22:40:13 +1100

    Update get-arnie-quotes.js

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,26 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const getArnieQuotes = (urls) => {
+    const promises = [];
+    urls.forEach(url => {
+        promises.push(httpGet(url));
+    });
+    return new Promise((resolve, reject) => {
+        Promise.all(promises).then((results) => {
+            const responses = [];
+            results.forEach((response) => {
+                const body = JSON.parse(response.body);
+                if (response.status === 200) {
+                    responses.push({ 'Arnie Quote': body.message })
+                } else {
+                    responses.push({ 'FAILURE': body.message })
+                }
+            });
+            resolve(responses);
+        });
+    });
 };
 
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
